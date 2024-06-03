43a21f26a2568be334a9bea871bcb0526e3d93ed
maia <matteusmaia889@gmail.com>
2024-06-03 18:11:57 -0300

    feat:  home base

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,48 @@
 import './index.css'
+import { FaTrashCan } from "react-icons/fa6";
 
 export function Home() {
 
+  const users = [
+    {
+    id: 'isodj01238012',
+    name: "Maia Mateues",
+    age: "20",
+    email: "maia@gmail.com"
+  },
+  {
+    id: 'isdoawdadj01238012',
+    name: "Amna",
+    age: "23",
+    email: "amna@gmail.com"
+  }
+]
+
   return (
-    <>
-      <h1> maia</h1>
-    </>
+    <div className='container'>
+      <form>
+        <h1>Cadastro de usuário </h1>
+        <input name='nome' type='text'/> 
+        <input name='idade' type='number'/> 
+        <input name='email' type='email'/> 
+        <button type="button"> Cadastrar</button>
+      </form>
+
+       {users.map(user => {
+        return(
+          <div key={user.id}>
+            <div>
+              <p>Nome: {user.name}</p>
+              <p>Idade: {user.age}</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <span>
+              <FaTrashCan size={20} /> 
+            </span>
+          </div>
+        )
+       })}
+    </div>
   )
 }
 
