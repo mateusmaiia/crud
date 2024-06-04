87396fdc292f43c2843f03451dfab5eac95cbeed
Maia <matteusmaia889@gmail.com>
2024-06-04 02:58:17 -0300

    feat: using zod and react-hook-form for the schema and done the CRUD

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -2,26 +2,52 @@ import './index.css'
 import { FaTrashCan } from "react-icons/fa6";
 import api from '../../services/api';
 import { useEffect, useState } from 'react';
+import z from 'zod'
+import { useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+
+const formSchema = z.object({
+  name: z.string(),
+  age: z.string(),
+  email: z.string().email(),
+})
 export function Home() {
   const [users, setUsers] = useState([])
+  const {register, handleSubmit} = useForm({
+    resolver: zodResolver(formSchema),
+  })
 
   async function getUsers(){
     const usersFetch = await api.get('/users')
     setUsers(usersFetch.data)
   }
 
+  async function createUsers(data){
+    const userCreated = await api.post("/users", {
+      name: data.name,
+      age: data.age,
+      email: data.email,
+    })
+    setUsers(prevUsers => [...prevUsers, userCreated.data])
+  }
+
+  async function deleteUser(id){
+    await api.delete(`/users/${id}`)
+  }
+
   useEffect(() => {
     getUsers()
-  }, [])
+  }, [users])
 
   return (
     <div className='container'>
       <form>
         <h1>Cadastro de usuário </h1>
-        <input placeholder='Nome:' name='nome' type='text'/> 
-        <input placeholder='Idade:' name='idade' type='number'/> 
-        <input placeholder='Email:' name='email' type='email'/> 
-        <button type="button"> Cadastrar</button>
+        <input placeholder='Nome:' name='nome' type='text' {...register("name")}/> 
+        <input placeholder='Idade:' name='idade' type='number' {...register("age")}/> 
+        <input placeholder='Email:' name='email' type='email' {...register("email")}/> 
+        <button type="button" onClick={handleSubmit(createUsers)}> Cadastrar</button>
       </form>
 
        {users.map(user => {
@@ -32,7 +58,7 @@ export function Home() {
               <p>Idade:<span>{user.age}</span> </p>
               <p>Email: <span>{user.email}</span></p>
             </div>
-            <button>
+            <button onClick={() => deleteUser(user.id)}>
               <FaTrashCan size={20} color='red' /> 
             </button>
           </div>
