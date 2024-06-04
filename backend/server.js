e91920ec12cc62cbe3add3f975c9a32876efd7dd
Maia <matteusmaia889@gmail.com>
2024-06-04 01:36:07 -0300

    feat: using cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import {PrismaClient} from '@prisma/client'
+import cors from 'cors'
+
 const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.post("/users",  (async (req, res) => {
 
